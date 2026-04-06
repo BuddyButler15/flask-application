be753753d0ddc17f171b5e259c3286aa104a478f
Jeffery Butler <jeffbutler@uiowa.edu>
2026-04-06 14:41:59 -0500

    bootstrap

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template   
+from flask import Flask, render_template
 
-app = Flask(__name__)                      
+app = Flask(__name__)
 
 @app.route("/")
-def home():                                
-    return render_template('index.html')   
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002, debug=True)  
+    app.run(debug=True)
